ee064b0ccfaf258e06ded0e01251cc28cbd656ea
Devin Wilson <craigvin84@yahoo.com>
2020-11-15 20:15:05 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,11 +2,11 @@ class Backer
 
   attr_reader :name, :backed_projects
 
-
+  @backed_projects = []
 
   def initialize(name)
     @name = name
-    @backed_projects = []
+
   end
 
   def back_project(project)
