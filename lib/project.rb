78c0641abb4e180f3836c8d9014e3266fea521bc
Devin Wilson <craigvin84@yahoo.com>
2020-11-15 20:33:25 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -2,7 +2,7 @@ class Project
 
   attr_accessor :title, :backers
 
-  @@backers = []
+  @backers = []
 
   def initialize(title)
     @title = title
@@ -10,7 +10,7 @@ class Project
   end
 
 def add_backer(backer)
-  @@backers << backer
+  @backers << backer
   backer.backed_projects << self
 end
 end
