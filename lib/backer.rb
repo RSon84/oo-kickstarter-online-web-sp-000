c0ccf0e0e4f65b60855101da9324b275f99364c5
Devin Wilson <craigvin84@yahoo.com>
2020-11-15 20:25:10 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,34 @@
-class Backer
+# class Backer
+# 
+#   attr_reader :name, :backed_projects
+# 
+# 
+# 
+#   def initialize(name)
+#     @name = name
+#     @backed_projects = []
+#   end
+# 
+#   def back_project(project)
+#     @backed_projects << project
+#   end
+# end
 
-  attr_reader :name, :backed_projects
+class Backer	
+  attr_reader :name, :backed_projects	
 
 
+  def initialize(name)	
+    @name = name	
+  end	
 
-  def initialize(name)
-    @name = name
-    @backed_projects = []
-  end
+  def backed_projects	
+    pbs = ProjectBacker.all.select {|pb| pb.backer == self}	
+    pbs.map {|pb| pb.project}	
+  end	
 
-  def back_project(project)
-    @backed_projects << project
-  end
+  def back_project(project)	
+    ProjectBacker.new(project, self)	
+  end	
+
 end
